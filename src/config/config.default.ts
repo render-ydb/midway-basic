f1718e244c3bb4030bffbe93dd8acabf2943d518
renderyang <renderyang@boyaa.com>
2024-12-21 16:04:43 +0800

    first commit

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1734768216539_762',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
